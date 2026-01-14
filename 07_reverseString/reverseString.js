464ea9ed84ea4a127d6ad0abfda5574fe0ef23d6
lester-mtse-machine <lesterc013@gmail.com>
2026-01-14 14:00:13 +0800

    Solve exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversed = "";
 
+    // Iterate from back of the string to the front
+    for (let i = str.length - 1; i >= 0; i--) {
+        // Concat each element to a variable
+        reversed += str[i]
+    }
+    // Return that variable
+    return reversed
 };
 
 // Do not edit below this line
