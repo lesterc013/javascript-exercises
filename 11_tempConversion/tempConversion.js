bdc6d2a0de2dcac9d154d3c92b0f48472d2a6e07
lester <lesterc013@gmail.com>
2026-01-14 21:53:21 +0800

    Solve ex 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  // Round to 1DP
+  let celsius = (farenheit - 32) / 1.8;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let farenheit = celsius * 1.8 + 32;
+  return Math.round(farenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
