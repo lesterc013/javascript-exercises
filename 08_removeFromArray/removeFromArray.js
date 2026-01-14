2fdd09bc6a48ad062531bba8b835d3449e973289
lester-mtse-machine <lesterc013@gmail.com>
2026-01-14 14:09:11 +0800

    Solve ex 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToRemove) {
+    // Create a set from elementsToRemove
+    const thingsToRemove = new Set(elementsToRemove)
+    const arrToReturn = arr.filter(e => !thingsToRemove.has(e))
+    return arrToReturn
 };
 
 // Do not edit below this line
