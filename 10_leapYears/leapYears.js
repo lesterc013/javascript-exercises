fff05c9461d8ca5c8495f7d72e10f41aae7630ce
lester <lesterc013@gmail.com>
2026-01-14 21:30:35 +0800

    Solve ex 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // If year can't even be div by 4, return false
+  if (year % 4 != 0) {
+    return false;
+  }
 
+  // If divisble by 4 but not by 100, then is leap year
+  if (year % 100 != 0) {
+    return true;
+  }
+
+  // If can be div by 4 AND 100, then leap year is true if it can also be div by 400.
+  if (year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
