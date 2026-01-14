6a5eb5e954a09eadc004d0e8a21be62e2618f7f6
lester-mtse-machine <lesterc013@gmail.com>
2026-01-14 13:56:46 +0800

    Completed all test cases

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(toRepeat, timesToRepeat) {
+    if (timesToRepeat < 0)
+        return "ERROR";
+    
+    let toReturn = "";
+    for (let i = 0; i < timesToRepeat; i++) {
+        toReturn += toRepeat;
+    }
+    return toReturn
 };
 
 // Do not edit below this line
