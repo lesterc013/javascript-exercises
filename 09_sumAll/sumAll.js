2c426ca33212665352a1ccdb098b96d644456839
lester-mtse-machine <lesterc013@gmail.com>
2026-01-14 14:26:09 +0800

    Solve ex 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) != "number" || typeof(b) != "number") {
+        return 'ERROR'
+    }
 
+    if (a < 0 || b < 0) {
+        return 'ERROR'
+    }
+
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR'
+    }
+
+    // Formula is (num of terms) * (a + b) / 2
+    return ((Math.abs(b - a) + 1)) * (a + b) / 2
 };
 
 // Do not edit below this line
